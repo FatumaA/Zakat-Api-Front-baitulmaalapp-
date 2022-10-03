912f9ab8a2d45122355ac2d85d1a5e884230cb23
FatumaA <67555014+FatumaA@users.noreply.github.com>
2022-10-03 16:22:40 +0300

    update routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,9 @@
 import './App.css';
-import ZakatForm from './form/ZakatForm';
-
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
-import { AuthContextProvider, useAuthState } from './firebase'
-
-import AdminArea from './adminArea/AdminArea';
-import SignUp from './loginRoutes/SignUp';
-import SignIn from './loginRoutes/SignIn';
-
-
-
-const AuthRoute = ({ component: C, ...props }) => {
-  const { isAuthenticated } = useAuthState()
-  console.log(`AuthenticatedRoute: ${isAuthenticated}`)
-  return (
-    <Route
-      {...props}
-      render={routeProps =>
-        isAuthenticated ? <C {...routeProps} /> : <Redirect to="/signin" />
-      }
-    />
-  )
-}
-
-const UnAuthRoute = ({ component: C, ...props }) => {
-  const { isAuthenticated } = useAuthState()
-  console.log(`UnauthenticatedRoute: ${isAuthenticated}`)
-  return (
-    <Route
-      {...props}
-      render={routeProps =>
-        // !isAuthenticated ? 
-        <C {...routeProps} /> 
-        // : <Redirect to="/" />
-      }
-    />
-  )
-}
-
-
-
+import WrapperForForm from './form/WrapperForForm';
 
 function App() {
   return (
-    <AuthContextProvider>
-    <Router>
-      <div color="secondary" className="flexIt">
-        <Link to="/" className='link'>Home</Link> 
-        <Link to="/signin" className='link'>SignIn</Link> 
-        <Link to="/signup" className='link'>SignUp</Link> 
-        <Link to="/admin" className='link'>Admin</Link> 
-      </div>
-      <UnAuthRoute exact path="/" component={ZakatForm} />
-      <AuthRoute  exact path="/admin" component={AdminArea}   />
-      <UnAuthRoute exact path="/signup" component={SignUp}   />
-      <UnAuthRoute  exact path="/signin" component={SignIn}   />
-    </Router>
-  </AuthContextProvider>
+    <WrapperForForm />
   );
 };
 
